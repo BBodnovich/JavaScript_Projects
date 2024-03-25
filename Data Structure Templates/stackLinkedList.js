aa17a70e021c0bbf48966115b889ab4a5e2bf317
DannSanchez <Bodnovich.BT@Gmail.com>
2024-03-25 09:50:38 -0400

    Initial stack built on Linked Lists

---
+++ Data Structure Templates/stackLinkedList.js
@@ -0,0 +1,77 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+class LinkedList {
+    constructor() {
+        this.top = null
+        this.bottom = null;
+        this.count = 0
+    }
+
+
+    push(value) {
+        const newNode = new Node(value);
+        if (this.count === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const topNode = this.top;
+            this.top.next = newNode;
+            this.top = newNode;
+        }
+
+        this.count ++;
+        return this.count;
+    }
+
+
+    pop() {
+        if (this.count === 0) {
+            return undefined;
+        }
+
+        const topNode = this.top
+        this.top = this.top.next;
+        topNode.next = null;
+
+        this.count --;
+        return topNode;
+    }
+
+
+    peek(index) {
+        return this.top;
+    }
+
+
+    length() {
+        return this.count;
+    }
+
+
+    isEmpty() {
+        return (this.count === 0);
+    }
+
+
+    lookup(index) {
+        if (index === 0 || index >= this.count) {
+            return undefined;
+        }
+
+        let currentNode = this.top;
+        let currentIndex = this.count - 1;
+
+        while (currentIndex > index) {
+            currentNode = currentNode.next;
+            currentIndex--;
+        }
+
+        return currentNode;
+    }
+}
